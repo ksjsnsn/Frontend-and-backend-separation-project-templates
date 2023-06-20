407adebb42435172d1e8d10a551df979defef3e9
yalin <linyaein@qq.com>
2023-06-20 12:10:29 +0800

    创建项目

---
+++ study-project-backend/src/main/java/com/prostudy/StudyProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyProjectBackendApplication.class, args);
+	}
+
+}
